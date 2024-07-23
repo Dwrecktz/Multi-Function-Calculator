60c4112c2918593f4a7344d20c80d287c694e0f2
Dwrecktz <declanward479@gmail.com>
2024-07-23 12:46:59 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,23 +6,23 @@ namespace MultiPurpose_Calculator
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Green; //Changes Text color to Green.
             Console.WriteLine("1 = Multiplication, 2 = Division, 3 = Addition, 4 = Subtraction");
-            int input = Convert.ToInt32(Console.ReadLine());
+            int input = Convert.ToInt32(Console.ReadLine()); //Takes number input, and converts it to an integer.
 
             switch(input)
             {
-                case 1:
+                case 1: //Checks if 1 is written.
                     Console.Write("Input first number: ");
-                    double firstNum = Convert.ToDouble(Console.ReadLine());
+                    double firstNum = Convert.ToDouble(Console.ReadLine()); // Converts written number text to double.
                     Console.Write("Input second number: ");
                     double secondNum = Convert.ToDouble(Console.ReadLine());
-                    double output = firstNum * secondNum;
+                    double output = firstNum * secondNum; //Does calculations.
                     Console.WriteLine("\n-----------------");
-                    Console.WriteLine(output);
+                    Console.WriteLine(output); //Writes the output.
                  break;
 
-                case 2:
+                case 2: //Checks if 2 is written.
                     Console.Write("Input first number: ");
                     double firstNum2 = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Input second number: ");
@@ -32,7 +32,7 @@ namespace MultiPurpose_Calculator
                     Console.WriteLine(output2);
                     break;
 
-                case 3:
+                case 3: //Checks if 3 is written.
                     Console.Write("Input first number: ");
                     double firstNum3 = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Input second number: ");
@@ -42,7 +42,7 @@ namespace MultiPurpose_Calculator
                     Console.WriteLine(output3);
                 break;
 
-                case 4:
+                case 4: //Checks if 4 is written.
                     Console.Write("Input first number: ");
                     double firstNum4 = Convert.ToDouble(Console.ReadLine());
                     Console.Write("Input second number: ");
